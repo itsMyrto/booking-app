3f5ee5849ac10c2207bc8777d7f8ece63490f551
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2022-01-06 15:42:08 +0200

    Delete src directory

--- src/UserProfileWindow.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class UserProfileWindow extends JPanel {
-    public UserProfileWindow(User user, AccountsCreated listOfAccounts, AccommodationsCreated listOfAccommodations){
-
-    }
-}
