ef98172afecdf3d269a2cc48757c1bd00c9785e4
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2022-01-08 00:39:43 +0200

    Add files via upload

--- src/ShowMessages.java
+++ src/ShowMessages.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class ShowMessages extends JPanel {
     public ShowMessages(User user, AccountsCreated listOfAccounts, AccommodationsCreated listOfAccommodations,ReservationsCreated listOfReservations,MainFrame mainFrame){
@@ -15,6 +17,25 @@ public class ShowMessages extends JPanel {
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
         scrollPane.setBounds(500,100,500,800);
         panel2.add(scrollPane);
+
+        JLabel inboxEmpty = new JLabel("Your inbox is empty");
+        inboxEmpty.setBounds(200,200,200,200);
+        inboxEmpty.setVisible(false);
+        panel2.add(inboxEmpty);
+
+        if(user.getMessages().size()==0){
+            inboxEmpty.setVisible(true);
+        }
+        else{
+            ArrayList<Message> messages = user.getMessages();
+            JLabel[] labels = new JLabel[messages.size()];
+            int count = 0;
+            for(Message x:messages){
+                labels[count].setText(x.getMessage());
+                panel2.add(labels[count]);
+                count++;
+            }
+        }
 
         add(panel2);
     }
