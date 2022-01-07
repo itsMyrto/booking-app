eedb8e391db4b1f34f814ee84bf03b72b2f428c3
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2022-01-08 00:39:43 +0200

    Add files via upload

---
+++ src/ShowMessagesAdmin.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ShowMessagesAdmin extends JPanel {
+    public ShowMessagesAdmin(User user, AccountsCreated listOfAccounts, AccommodationsCreated listOfAccommodations,ReservationsCreated listOfReservations,MainFrame mainFrame){
+        setSize(1500,800);
+        setLayout(null);
+
+
+    }
+}
