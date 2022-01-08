c72996dd59fef43ef4251533a3c1c8700965b4da
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2022-01-08 20:29:53 +0200

    Update ShowMessages.java

--- src/ShowMessages.java
+++ src/ShowMessages.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 
 public class ShowMessages extends JPanel {
@@ -7,12 +9,6 @@ public class ShowMessages extends JPanel {
     private final GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
     private final int PANEL_WIDTH = (int) (env.getMaximumWindowBounds().getWidth());
     private final int PANEL_HEIGHT = (int) (env.getMaximumWindowBounds().getHeight());
-
-    private JScrollPane scrollPane = new JScrollPane();
-
-    private ArrayList<JLabel> messagesLabel = new ArrayList<>();
-
-    private ArrayList<Message> messages = new ArrayList<>();
 
     private final int PANE_WIDTH = (int) (0.8*PANEL_WIDTH);
     private final int PANE_HEIGHT = (int) (0.7*PANEL_HEIGHT);
@@ -27,26 +23,22 @@ public class ShowMessages extends JPanel {
     private final String FONT = "Tahoma";
     private final Color CUSTOMIZED_COLOR = Color.decode("#3B5998");
 
-    public ShowMessages(User user, AccountsCreated listOfAccounts, AccommodationsCreated listOfAccommodations,ReservationsCreated listOfReservations,MainFrame mainFrame){
-        this.setSize(PANEL_WIDTH,PANEL_HEIGHT);
 
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(MARGIN_PANE_FROM_LEFT_EDGE,MARGIN_PANE_FROM_TOP,PANE_WIDTH,PANE_HEIGHT);
-        scrollPane.setForeground(CUSTOMIZED_COLOR);
-        scrollPane.setBackground(Color.WHITE);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+    public ShowMessages(User user, AccountsCreated listOfAccounts, AccommodationsCreated listOfAccommodations,ReservationsCreated listOfReservations,MainFrame mainFrame){
+
+        this.setSize(PANEL_WIDTH,PANEL_HEIGHT);
+        mainFrame.setVisible(false);
 
         int pxCounter = 0;
         int counter = 0;
 
-        for(int i=0; i<50; i++){
-            Message msg1 = new Message("Hello, " + i, new User("Gregory", "1", "g", "g", "Tsoum"));
-            messages.add(msg1);
-        }
+        ArrayList<Message> messages = user.getMessages();
+      
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0,1));
 
         for(Message x : messages){
-            String msg = MARGIN_MSG_FROM_PANE_EDGE + x.getMessage();
+            String msg = MARGIN_MSG_FROM_PANE_EDGE + x.getMessage() + "   From: "+x.getSender().getEmail();
             JLabel temp = new JLabel(msg);
             if(counter % 2 == 0) {
                 temp.setOpaque(true);
@@ -62,26 +54,31 @@ public class ShowMessages extends JPanel {
             pxCounter += MSG_LABEL_HEIGHT;
             counter++;
 
-            scrollPane.add(temp);
+            panel.add(temp);
         }
 
-        if(user.getMessages().size()==0){
+        panel.setSize(new Dimension(PANE_WIDTH,PANE_HEIGHT));
 
-        }
-        else{
-            ArrayList<Message> messages = user.getMessages();
-            JLabel[] labels = new JLabel[messages.size()];
-            int count = 0;
-            for(Message x:messages){
-                labels[count].setText(x.getMessage());
-                count++;
-            }
-        }
+
+        JScrollPane scrollPane = new JScrollPane(panel);
+
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setBounds(MARGIN_PANE_FROM_LEFT_EDGE,MARGIN_PANE_FROM_TOP,PANE_WIDTH,PANE_HEIGHT);
+        scrollPane.setForeground(CUSTOMIZED_COLOR);
+        scrollPane.setBackground(Color.WHITE);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
         this.add(scrollPane);
 
         this.setOpaque(false);
         this.setLayout(null);
+
+        panel.setVisible(true);
         this.setVisible(true);
+
+        mainFrame.setVisible(true);
+
     }
 }
+
