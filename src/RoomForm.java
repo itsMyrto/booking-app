e98ad82e02a63c09681fc65ac007b88fa0894631
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2022-01-16 22:10:31 +0200

    Cleaner Code

--- src/RoomForm.java
+++ src/RoomForm.java
@@ -2,36 +2,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * This class contains the panel that shows a form where a provider can add as many rooms as he/she wants for the new accommodation.
+ * For each room is necessary to fill some fields like price,capacity,number of beds (4 people - 2 double beds or 4 single beds) & the view of the room
+ */
 public class RoomForm extends JPanel {
     public RoomForm(Accommodation accommodation,AccommodationsCreated listOfAccommodations,AccountsCreated listOfAccounts,ReservationsCreated listOfReservations,MainFrame mainFrame){
+
         setSize(1500,800);
+
         AtomicBoolean roomsAdded = new AtomicBoolean(false);
+
         JLabel filterLabel = new JLabel("Which view does the room have?");
-        JLabel notice = new JLabel("Please fill the below:");
-        JLabel note = new JLabel("You can add as many rooms as you want");
-        note.setFont(new Font("Sans Sheriff", Font.ITALIC+Font.BOLD, 15));
         filterLabel.setFont(new Font("Sans Sheriff", Font.ITALIC+Font.BOLD, 17));
         filterLabel.setForeground(new Color(0x06307C));
+        filterLabel.setBounds(230,300,300,30);
+
+        JLabel notice = new JLabel("Please fill the below:");
         notice.setFont(new Font("Sans Sheriff", Font.ITALIC+Font.BOLD, 17));
         notice.setForeground(new Color(0x06307C));
         notice.setBounds(260,100,200,30);
-        filterLabel.setBounds(230,300,300,30);
+
+        JLabel note = new JLabel("You can add as many rooms as you want");
+        note.setFont(new Font("Sans Sheriff", Font.ITALIC+Font.BOLD, 15));
         note.setBounds(200,500,500,30);
+
+
         JButton addRoom = new JButton("Add Room");
-        JButton finish = new JButton("Finish");
-        JLabel error = new JLabel("Invalid Information");
-        finish.setBackground(new Color(0x06307C));
-        finish.setForeground(Color.white);
-        error.setForeground(Color.red);
-        error.setFont(new Font("sans serif",Font.ITALIC+Font.BOLD,14));
-        addRoom.setFocusable(false);
-        finish.setFocusable(false);
         addRoom.setFont(new Font("sans serif",Font.ITALIC+Font.BOLD,14));
         addRoom.setBackground(new Color(0x06307C));
         addRoom.setForeground(Color.white);
+        addRoom.setFocusable(false);
+        addRoom.setBounds(250,450,200,25);
+
+        JButton finish = new JButton("Finish");
+        finish.setBackground(new Color(0x06307C));
+        finish.setForeground(Color.white);
+        finish.setFocusable(false);
+        finish.setBounds(250,650,200,25);
+
+        JLabel error = new JLabel("Invalid Information");
+        error.setForeground(Color.red);
+        error.setFont(new Font("sans serif",Font.ITALIC+Font.BOLD,14));
+
+
         JTextField capacity = new JTextField("Room Capacity");
+        capacity.setBounds(250,150,200,25);
+
         JTextField price = new JTextField("Price For One Night");
+        price.setBounds(250,200,200,25);
+
         JTextField beds = new JTextField("Number of Beds");
+        beds.setBounds(250,250,200,25);
+
         JComboBox<String> view;
         String[] viewType = {"Sea","Forest","The city","Landmark","Lake"};
         view = new JComboBox<>(viewType){
@@ -47,13 +70,9 @@ public class RoomForm extends JPanel {
                 }
             }
         };
+
         view.setFont(new Font("Sans Sheriff", Font.ITALIC+Font.BOLD, 12));
-        capacity.setBounds(250,150,200,25);
-        price.setBounds(250,200,200,25);
-        beds.setBounds(250,250,200,25);
         view.setBounds(250,350,200,25);
-        addRoom.setBounds(250,450,200,25);
-        finish.setBounds(250,650,200,25);
         view.setSelectedIndex(0);
 
         addRoom.addActionListener(e -> {
@@ -71,7 +90,6 @@ public class RoomForm extends JPanel {
             } catch(Exception er){
                 error.setText("Invalid Information");
                 error.setBounds(300,550,200,30);
-                //er.printStackTrace();
                 return;
             }
             if(!(capacityTxt.isEmpty() && bedsTxt.isEmpty() && priceTxt.isEmpty())){
@@ -100,6 +118,7 @@ public class RoomForm extends JPanel {
 
 
         });
+
 
         add(note);
         add(error);
